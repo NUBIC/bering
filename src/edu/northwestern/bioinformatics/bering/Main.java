a68dfa93b3cdbd6449604cfb35f52017c0227746
Rhett Sutphin <rhett@detailedbalance.net>
2006-06-30 18:37:08 +0000

    Commit latest version so Moses can work on the ant task

--- src/edu/northwestern/bioinformatics/bering/Main.java
+++ src/edu/northwestern/bioinformatics/bering/Main.java
@@ -1,10 +1,8 @@
 package edu.northwestern.bioinformatics.bering;
 
 import edu.northwestern.bioinformatics.bering.runtime.MigrationFinder;
-import edu.northwestern.bioinformatics.bering.runtime.MigrationLoadingException;
-import edu.northwestern.bioinformatics.bering.runtime.Release;
-import edu.northwestern.bioinformatics.bering.runtime.Script;
 import edu.northwestern.bioinformatics.bering.runtime.Version;
+import edu.northwestern.bioinformatics.bering.runtime.Target;
 
 import java.io.File;
 
@@ -20,6 +18,9 @@ public class Main {
      * If <code>targetMigrationNumber</code> is null, the database will be migrated to the
      * last script in <code>targetReleaseNumber</code>.  If <code>targetReleaseNumber</code>
      * is null, the target will be the latest release.
+     * <p>
+     *
+     * </p>
      *
      * @param targetReleaseNumber
      * @param targetMigrationNumber
@@ -35,46 +36,19 @@ public class Main {
         Version current = adapter.loadVersions();
 
         // walk through scripts, running ones that haven't been run, up to / down to desired release/version
-
-
-    }
-
-    private static class Target {
-        private int releaseNumber, migrationNumber;
-
-        private Target(int releaseNumber, int migrationNumber) {
-            this.releaseNumber = releaseNumber;
-            this.migrationNumber = migrationNumber;
-        }
-
-        public static Target create(MigrationFinder finder, Integer requestedReleaseNumber, Integer requestedMigrationNumber) {
-            int relN = requestedReleaseNumber == null
-                ? finder.getMaxReleaseNumber()
-                : requestedReleaseNumber;
-
-            Release requestedRelease = finder.getRelease(relN);
-            if (requestedRelease == null) {
-                throw new MigrationLoadingException("There's no release number " + requestedMigrationNumber);
-            }
-
-            int migN = requestedMigrationNumber == null
-                ? requestedRelease.getMaxScriptNumber()
-                : requestedReleaseNumber;
-
-            Script requestedMigrationScript = requestedRelease.getScript(migN);
-            if (requestedMigrationScript != null) {
-                throw new MigrationLoadingException("There's no migration number " + requestedMigrationNumber + " in release " + requestedReleaseNumber);
-            }
-
-            return new Target(relN, migN);
-        }
-
-        public int getReleaseNumber() {
-            return releaseNumber;
-        }
-
-        public int getMigrationNumber() {
-            return migrationNumber;
+        if (target.getReleaseNumber() >= current.getReleaseNumbers().last()) {
+            // TODO
         }
     }
+
+    ////// CONFIGURATION
+
+    public void setRootDir(String rootDir) {
+        this.rootDir = rootDir;
+    }
+
+    public void setAdapter(Adapter adapter) {
+        this.adapter = adapter;
+    }
+
 }
