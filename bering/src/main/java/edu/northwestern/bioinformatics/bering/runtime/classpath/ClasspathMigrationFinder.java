7d3469219d9b7c35f5458fe2a5cda9753f2bdbb3
Rhett Sutphin <rhett@detailedbalance.net>
2007-03-25 19:18:26 +0000

    Add more logging/exception handling

--- bering/src/main/java/edu/northwestern/bioinformatics/bering/runtime/classpath/ClasspathMigrationFinder.java
+++ bering/src/main/java/edu/northwestern/bioinformatics/bering/runtime/classpath/ClasspathMigrationFinder.java
@@ -7,6 +7,8 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.core.io.Resource;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.io.IOException;
 import java.util.Map;
@@ -16,6 +18,8 @@ import java.util.HashMap;
  * @author Rhett Sutphin
  */
 public class ClasspathMigrationFinder extends AbstractMigrationFinder {
+    private static final Log log = LogFactory.getLog(ClasspathMigrationFinder.class);
+
     private String rootResourcePath;
     private ResourcePatternResolver resolver;
 
@@ -28,18 +32,36 @@ public class ClasspathMigrationFinder extends AbstractMigrationFinder {
     private void initialize() {
         String pattern = createPattern();
         try {
-            Resource[] resources = resolver.getResources(pattern);
+            Resource[] resources = getResources(pattern);
             Release[] releases = new ReleaseFactory(resources).createReleases();
             for (Release release : releases) {
                 addRelease(release);
             }
         } catch (IOException e) {
-            throw new MigrationLoadingException("Could not find migration scripts using pattern " + pattern, e);
+            throw new MigrationLoadingException(
+                "Could not find migration scripts using pattern " + pattern, e);
         }
     }
 
+    private Resource[] getResources(String pattern) throws IOException {
+        if (log.isDebugEnabled()) log.debug("Looking for resources matching " + pattern);
+        Resource[] resources = resolver.getResources(pattern);
+        if (resources.length == 0) {
+            throw new MigrationLoadingException(
+                "No migration scripts found using pattern " + pattern);
+        }
+
+        log.info(resources.length + " bering script resource(s) found matching " + pattern);
+        if (log.isDebugEnabled()) {
+            for (int i = 0; i < resources.length; i++) {
+                log.debug(" " + i + ") " + resources[i]);
+            }
+        }
+        return resources;
+    }
+
     private String createPattern() {
-        return String.format("classpath*:%s%s/*/*.groovy",
+        return String.format("classpath*:%s%s*/*.groovy",
             rootResourcePath,
             rootResourcePath.endsWith("/") ? "" : "/");
     }
